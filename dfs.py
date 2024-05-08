964c927c8b78c69d8c4aecd3c7f7ef316a6c6286
jaysutar001 <156189085+jaysutar001@users.noreply.github.com>
2024-05-09 00:40:51 +0530

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,19 @@
+g = {
+    0:[1,2],
+    1:[0,3,4],
+    2:[0,3],
+    3:[1,2,4],
+    4:[3,1]
+}
+
+def dfs(g,s):
+    vis[s] = 1
+    print(s)
+    for c in g[s]:
+        if not vis[c]:
+            dfs(g,c)
+
+
+vis = [0]*5
+
+dfs(g,0)
